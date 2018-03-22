5efa68c163806e43c219a2ae90829148c2097f72
christoph.maeder <christoph.maeder@isolutions.ch>
2018-03-22 13:58:39 +0100

    Initial add of MetaDataFunctions.ps1

---
+++ Dynamics 365 CE/MetaDataFunctions.ps1
@@ -0,0 +1,34 @@
+
+# In this library:
+# 
+# GetAllSolutionsDataToCsv -CsvPath "C:\extreme\Employees.csv"
+
+function GetAllSolutionsDataToCsv()
+{
+    param(
+        [string]$CsvPath
+    )
+
+    # IMPORT MODULE
+    Import-Module Microsoft.Xrm.Data.Powershell
+
+    # CREATE A CRM CONNECTION
+    Connect-CrmOnlineDiscovery -InteractiveMode
+
+
+    $solutions = Get-CrmRecords -EntityLogicalName solution -Fields uniquename,friendlyname,ismanaged #-FilterAttribute uniquename -FilterOperator eq -FilterValue $solutionName -Fields uniquename,version,ismanaged
+   
+    Add-Content -Path $CsvPath -Value "DisyplayName;UniqueName;IsManaged"
+
+    foreach($solution in $solutions.CrmRecords)
+    {
+        $uname = $solution.uniquename
+        $disname = $solution.friendlyname
+        $ismanaged = $solution.ismanaged.ToString()
+
+        Write-Host "$disname;$uname;$ismanaged"
+        Add-Content -Path C:\extreme\Employees.csv  -Value "$disname;$uname;$ismanaged"
+    }
+}
+
+#GetAllSolutionsDataToCsv -CsvPath "C:\extreme\Employees.csv"
