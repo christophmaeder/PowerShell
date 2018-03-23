c9be9be40e40c9370e2e2932cbdad2e2cbfd1de5
christophmaeder <christoph.maeder@procrm.ch>
2018-03-23 08:13:46 +0100

    Added some new functions.

--- Dynamics 365 CE/CrmConnections.ps1
+++ Dynamics 365 CE/CrmConnections.ps1
@@ -1,23 +1,30 @@
 
-##### BEGIN Creating a connection to Dynaics 365 CE  #####
+# IMPORT MODULES
+Import-Module Microsoft.Xrm.Data.Powershell
 
-    # IMPORT MODULES
-    Import-Module Microsoft.Xrm.OnlineManagementAPI
-    Import-Module Microsoft.Xrm.Data.Powershell
-
-    # IMPORT FUNCTION FILES
-    #. "C:\Users\Admin\Documents\Visual Studio 2017\Code Snippets\PowerShell\Helpers\CrmConnection.ps1"
-
-    # INIT CONNECTION VARIABLES                
-    $ApiUrl = "https://admin.services.crm4.dynamics.com"
-    $CrmServerUrl = "https://xy.crm4.dynamics.com"
-
-    # CREATE CREDENTIALS
-    $Username = ""
-    $Password = Read-host "Password:" -AsSecureString 
-    $Cred = New-Object System.Management.Automation.PSCredential ($Username, $Password)
-    
+function InitCrmConnectionInteractive() 
+{ 
     # CREATE A CRM CONNECTION
-    Connect-CrmOnline -Credential $Cred -ServerUrl $CrmServerUrl # Used by "Microsoft.Xrm.Data.Powershell" cmdlets
-    
-##### END Creating a connection to Dynaics 365 CE #####
+    Connect-CrmOnlineDiscovery -InteractiveMode
+}
+
+function InitCrmConnection()
+{ 
+    param(
+        [System.Management.Automation.PSCredential]$CrmCredentials   ,
+        [string]$CrmServerUrl     
+    )
+
+    # CREATE A CRM CONNECTION
+    Connect-CrmOnline -Credential $CrmCredentials -ServerUrl $CrmServerUrl 
+}
+
+function GetPsCredentials()
+{
+    param(
+        [string]$Username,
+        [securestring]$Password      
+    )
+
+    return New-Object System.Management.Automation.PSCredential ($Username, $Password)
+}
--- Dynamics 365 CE/MetaDataFunctions.ps1
+++ Dynamics 365 CE/MetaDataFunctions.ps1
@@ -3,20 +3,20 @@
 # 
 # GetAllSolutionsDataToCsv -CsvPath "C:\temp\solutions.csv"
 
-function GetAllSolutionsDataToCsv()
+# IMPORT MODULE
+Import-Module Microsoft.Xrm.Data.Powershell
+
+function ExportAllSolutionDataToCsv()
 {
     param(
         [string]$CsvPath
     )
 
-    # IMPORT MODULE
-    Import-Module Microsoft.Xrm.Data.Powershell
-
-    # CREATE A CRM CONNECTION
-    Connect-CrmOnlineDiscovery -InteractiveMode
+    Write-Host "Getting solutions and creating CSV file ..."
+    Write-Host ""
 
     $solutions = Get-CrmRecords -EntityLogicalName solution -Fields uniquename,friendlyname,ismanaged
-
+    
     Add-Content -Path $CsvPath -Value "DisyplayName;UniqueName;IsManaged"
 
     foreach($solution in $solutions.CrmRecords)
@@ -28,4 +28,72 @@ function GetAllSolutionsDataToCsv()
         Write-Host "$disname;$uname;$ismanaged"
         Add-Content -Path $CsvPath  -Value "$disname;$uname;$ismanaged"
     }
+
+    Write-Host ""
+    Write-Host "Finished getting and exporting solution data."
+}
+function CreatePublisherAndDefaultSolution() 
+{    
+    param (   
+        [string]$publishername,
+        [string]$publisherlogicalname,
+        [string]$publisherprefix,
+        [string]$solutionname,
+        [string]$solutionlogicalname
+    )
+
+    Write-Output "Creating solution and publisher ..."
+    Write-Output ""
+
+    # CREATE Publisher
+    $publishers = Get-CrmRecords -EntityLogicalName publisher -FilterAttribute uniquename -FilterOperator eq -FilterValue $publisherlogicalname
+    
+    if($publishers.Count -eq 0)
+    {
+        $publisherid = New-CrmRecord -EntityLogicalName publisher -Fields @{"uniquename"=$publisherlogicalname;"friendlyname"=$publishername;"customizationprefix"=$publisherprefix}
+    }
+    else
+    {
+        $publisherid = $publishers.CrmRecords[0].publisherid    
+    }
+
+    Write-Output "Publisher with id = $publisherid found or created ..."
+
+    # CREATE SOLUTION
+    $publisherref = New-CrmEntityReference -EntityLogicalName publisher -Id $publisherid
+
+    $solutionid = New-CrmRecord -EntityLogicalName solution -Fields @{"uniquename"=$solutionlogicalname;"friendlyname"=$solutionname;"publisherid"=$publisherref;"version"="1.0.0.0"}
+
+    Write-Output ""
+    Write-Output "... solution created ($solutionid)."
+}
+
+function CreateNewAutonumberAttribute () {
+    
+    param (   
+        [string]$AttributeName,
+        [string]$AttributeDisplayname,
+        [string]$EntitySchemaname,
+        [string]$AutoNumberFormat
+    )
+
+    Write-Output "Creating autonumber attribute $attributename ..."
+    Write-Output ""
+
+    $autonumattribute = New-Object Microsoft.Xrm.Sdk.Metadata.StringAttributeMetadata
+    $autonumattribute.SchemaName = $attributename
+    $autonumattribute.LogicalName = $attributename
+    $autonumattribute.DisplayName = New-Object Microsoft.Xrm.Sdk.Label($attributedisplayname,1033)
+    $autonumattribute.Format = [Microsoft.Xrm.Sdk.Metadata.StringFormat]::Text
+    $autonumattribute.MaxLength = 100
+    $autonumattribute.AutoNumberFormat = "{SEQNUM:6}"
+
+    $request = New-Object Microsoft.Xrm.Sdk.Messages.CreateAttributeRequest
+    $request.Attribute = $autonumattribute
+    $request.EntityName = $entityschemaname
+
+    $conn.Execute($request)
+
+    Write-Output ""
+    Write-Output "... autonumber attribute $attributename created."
 }
